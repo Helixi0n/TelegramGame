40a5c76699071c5bd19c0bc4e6df94b570d8a341
Helixi0n <111045175+Helixi0n@users.noreply.github.com>
2025-11-22 19:40:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from controllers import Controller
 
 # Главный файл, запустить для начала работы бота
 
-bot = TeleBot('8276363231:AAGvbsP7IiV08lPBza7aAV_rjKlE8C8OXTI')
+bot = TeleBot('')
 controller = Controller(bot)
 
 controller.register_handlers()
 
 if __name__ == "__main__":
     print("Bot is running...")
+
 
     bot.polling(none_stop=True)
